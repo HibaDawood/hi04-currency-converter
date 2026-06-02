b6d0d25570401543f80007c1bc63cd7c6f6c43fd
Hiba Dawood <164556977+HibaDawood@users.noreply.github.com>
2026-06-02 16:25:01 +0500

    Fix comments and formatting in index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
+
 console.log(chalk.yellowBright.italic.bold("\n \tWelcome to 'Hiba M.Dawood' - Currency-converter\n"));
-//Define the list of currencies and thier exchange rate
+
+// Define the list of currencies and their exchange rate
 let exchange_rate = {
-    USD: 1, //Base currency
-    YEN: 113, //Japanees currency (yen)
-    EUR: 0.9, //European currency(Euro)
-    CAD: 1.3, //Canadian Dollar
-    AUD: 1.65, //Australian Dollar
-    PKR: 277.42, //Pakistan Rupees
+    USD: 1,      // Base currency
+    YEN: 113,    // Japanese currency (yen)
+    EUR: 0.9,    // European currency(Euro)
+    CAD: 1.3,    // Canadian Dollar
+    AUD: 1.65,   // Australian Dollar
+    PKR: 277.42, // Pakistan Rupees
 };
-//propmt the user to select currencies from and to
+
+// Prompt the user to select currencies from and to
 let user_answer = await inquirer.prompt([
     {
         name: "from_currency",
@@ -31,12 +34,17 @@ let user_answer = await inquirer.prompt([
         message: chalk.magenta("Enter the amount to convert:"),
     },
 ]);
-//perform currency conversation by using formula
+
+// Perform currency conversion by using formula
 let from_amount = exchange_rate[user_answer.from_currency];
 let to_amount = exchange_rate[user_answer.to_currency];
-let amount = user_answer.amount;
-//Formula of conversation
+
+// ✅ user_answer.amount ko string se floating-point number mein convert kiya
+let amount = parseFloat(user_answer.amount); 
+
+// Formula of conversion
 let base_amount = amount / from_amount;
 let converted_amount = base_amount * to_amount;
-//Disply the converted
+
+// Display the converted amount
 console.log(`Conveted amount = ${chalk.green(converted_amount.toFixed(2))}`);
